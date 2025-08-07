f4511515738b3f76c11086b6db285569eeb01d0c
REMO <84759212+RemoDeveloper@users.noreply.github.com>
2025-08-07 12:24:09 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
     <section className="h-wrapper" style={{ background: headerColor }}>
       <div className="flexCenter innerWidth paddings h-container">
         {/* logo */}
-        <img src="/logo-cityNest-01.png" alt="logo" width={100} />
+        <img src="logo-cityNest-01.png" alt="logo" width={100} />
 
         {/* menu */}
         <OutsideClickHandler
